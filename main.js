275d980fd083055fe74e880c841ae255be5cc4f1
Thomas LaFreniere <thomas@lafreniere.co>
2014-07-26 14:39:20 -0700

    fixed bot & enhanced set page

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
+var fs = require('fs');
 
-var latest = 'http://www.google.ca';
+var debug = fs.existsSync('debug');
+
+if (debug) console.log('Starting in debug mode');
+
+var urlregex = /\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|$!:,.;]*[A-Z0-9+&@#\/%=~_|$]/i;
+var latest = 'http://i.imgur.com/2MuKYuw.gif';
 
 //---
 
@@ -18,7 +24,7 @@ var server =  app.listen(3000, function() {
 });
 
 app.get('/show', function(req, res) {
-	res.send('<html><body><form action="/show" method="POST"><label>URL</label><input name="url" id="url" type="text" /><input type="submit" value="Show" /></form></body></html>');
+    res.render('set', { latest: latest });
 });
 
 app.post('/show', function(req, res) {
@@ -39,10 +45,14 @@ app.get('/latest', function(req, res) {
 
 //---
 
+var ircserver = 'irc.oftc.net';
+var nick = (debug) ? "vhstv-debug" : "vhstv";
+var channel = (debug) ? "#vhs-debug" : "#vhs";
+
 var irc = require("irc");
 
-var bot = new irc.Client('irc.oftc.net', 'vhstv', {
-    channels: ['#vhs']
+var bot = new irc.Client(ircserver, nick, {
+    channels: [channel]
 });
 
 bot.addListener('error', function(message) {
@@ -51,28 +61,33 @@ bot.addListener('error', function(message) {
 
 bot.addListener('message', function (from, to, message) {
     console.log('%s => %s: %s', from, to, message);
-
-    if ( to.match(/^[#&]/) ) {
-        // channel message
-        if ( message.match(/http/i) ) {
-			var url = message.match(/((https?):\/\/)?[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]$/i);
-			if (url) {
-				url = url[0];
-				console.log('launching: %j', url);
-				latest = url;
-				bot.say(to, 'Showing ' + url);
-			} else {
-				bot.say(to, 'seriously not going to show that.');
-			}
-        }
-        if ( message.match(/dance/) ) {
+    
+    var handleChat = function(message, to) {
+        var url = message.match(urlregex);
+        if (url) {
+            url = url[0];
+            console.log('launching: %j', url);
+            latest = url;
+            bot.say(to, 'Showing ' + url);
+        } else if (message.match(/dance/)) {
             setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D\\-<\u0001") }, 1000);
             setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 2000);
             setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D/-<\u0001")  }, 3000);
             setTimeout(function () { bot.say(to, "\u0001ACTION dances: :D|-<\u0001")  }, 4000);
+        } else {
+            bot.say(to, "I don't know what to say about that..");
         }
-    }
-    else {
+    };
+
+    if (to.match(/^[#&]/)) {
+        // channel message
+        if (message.match("^"+nick)) {
+            handleChat(message, to);
+        }
+    } else {
         // private message
+        if (to.match("^"+nick)) {
+            handleChat(message, from);
+        }
     }
 });
